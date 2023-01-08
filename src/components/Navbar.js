50123015baf01bbf810cfa5beefacc1c5e5b547e
David <doa9200@gmail.com>
2023-01-08 18:41:57 -0500

    navbar finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose} from 'react-icons/md';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [toggleDrop, setToggleDrop] = useState(false);
+    const [toggleTeams, setToggleTeams] = useState(false);
+
     return (
         <header className="bg-gray-900 py-4 lg:py-6">
             <div className="container mx-auto px-4 lg:px-8">
@@ -15,6 +20,7 @@ const Navbar = () => {
                         </Link>
                     </div>
 
+                    {/* Desktop/Tablet View Menu */}
                     <div className="hidden md:block">
                         <nav className="flex items-center">
                             <Link to="/" className="text-white font-semibold py-3 px-4 hover:bg-gray-800 rounded-md">
@@ -25,22 +31,22 @@ const Navbar = () => {
                             </Link>
 
                             <div className="relative">
-                                <button className="text-white font-semibold py-3 px-4 hover:bg-gray-800 rounded-md">
+                                <button onClick={()=> setToggleDrop(!toggleDrop)} className="text-white font-semibold py-3 px-4 hover:bg-gray-800 rounded-md">
                                 Teams
                                 </button>
-                                <ul className="absolute hidden w-48 pt-2 bg-gray-900 rounded-md shadow-xl">
+                                <ul className={`absolute ${toggleDrop ? "" : "hidden"} w-48 pt-2 bg-gray-900 rounded-md`}>
                                     <li>
                                         <Link to="/teams/u23" className="block px-4 py-2 text-sm font-semibold text-white hover:bg-gray-800 rounded-md">
                                         U23 Team
                                         </Link>
                                     </li>
                                     <li>
-                                        <Link to="/teams/womens" className="block px-4 py-2 text-sm font-semibold text-white hover:bg-gray-800 rounded-md">
+                                        <Link to="/teams/women" className="block px-4 py-2 text-sm font-semibold text-white hover:bg-gray-800 rounded-md">
                                         Women's Team
                                         </Link>
                                     </li>
                                     <li>
-                                        <Link to="/teams/tryouts" className="block px-4 py-2 text-sm font-semibold text-white hover:bg-gray-800 rounded-md">
+                                        <Link to="/tryouts" className="block px-4 py-2 text-sm font-semibold text-white hover:bg-gray-800 rounded-md">
                                         Tryout Info
                                         </Link>
                                     </li>
@@ -56,12 +62,45 @@ const Navbar = () => {
                         </nav>
                     </div>
 
-                    <div className="block md:hidden">
-                        <button className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
-                            <GiHamburgerMenu />
-                        </button>
-                    </div>
+                    {/* Mobile Hamburger Menu */}
+                    <div className="sm:flex md:hidden">
+                        <GiHamburgerMenu fontSize={27} onClick={()=> setToggleMenu(true)} className="cursor-pointer text-white hover:text-red-500" />
 
+                        {toggleMenu && (
+                        <div className='flex justify-center items-center slide-bottom fixed top-0 left-0 w-full h-screen bg-gray-900 z-[5]'>
+                            {!toggleTeams ?
+                                (<MdClose fontSize={27} className='text-white absolute top-5 right-5 cursor-pointer' onClick={() => setToggleMenu(false)} />)
+                                :
+                                (<MdClose fontSize={27} className='text-white absolute top-5 right-5 cursor-pointer' onClick={() => setToggleTeams(!toggleTeams)} />)
+                            }
+
+                            {!toggleTeams ? 
+                                (
+                                    <ul className='flex-1 flex justify-center flex-col items-center text-white'>
+                                        <Link to='/' className='text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500' onClick={() => setToggleMenu(false)}>Home</Link>
+                                        <Link to='/about' className='text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500' onClick={() => setToggleMenu(false)}>About</Link>
+                                        <li className='text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500 cursor-pointer' onClick={() => setToggleTeams(!toggleTeams)}>Teams</li>
+                                            
+                                        <Link to='/shop' className='text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500' onClick={() => setToggleMenu(false)}>Shop</Link>
+                                        <Link to='/contact' className='text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500' onClick={() => setToggleMenu(false)}>Contact</Link>
+                                    </ul>
+                                ) 
+                            :
+                                (
+                                    <ul className='flex-1 flex justify-center flex-col items-center text-white slide-bottom'>
+                                        <li className='text-center text-[1.5rem] sm:text-[2rem] w-full mt-8 py-4 text-black hover:text-red-500 cursor-pointer bg-slate-300' onClick={() => setToggleTeams(!toggleTeams)}>
+                                            Teams
+                                        </li>
+                                        <Link to='/teams/u23' className={`block ${toggleTeams ? "" : "hidden"} text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500`} onClick={() => setToggleMenu(false)}>U23 Team</Link>
+                                        <Link to='/teams/women' className={`block ${toggleTeams ? "" : "hidden"} text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500`} onClick={() => setToggleMenu(false)}>Women's Team</Link>
+                                        <Link to='/tryouts' className={`block ${toggleTeams ? "" : "hidden"} text-center text-[1.5rem] sm:text-[2rem] m-8 hover:text-red-500`} onClick={() => setToggleMenu(false)}>Tryout Info</Link>
+                                    </ul>
+                                )
+                            }
+                        </div>
+                        )}                        
+                    </div>
+                    
                 </div>
             </div>
         </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '450px',
+      md: '650px',
+      ml: '850px',
+      lg: '1150px',
+      xl: '2750px',
+    },
+    extend: {
+      fontFamily: {
+        base: ["Cormorant Upright", "serif"],
+        alt: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        'burgandy': '#6A040F',
+      },
+    },
   },
   plugins: [],
 }
